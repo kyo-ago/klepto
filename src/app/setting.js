7f48031976b46de7c66b06d0e95f962d01b8265a
kyo_ago <kyo@0-9.sakura.ne.jp>
2013-02-24 04:50:04 +0900

    add "Save success." message

--- src/app/setting.js
+++ src/app/setting.js
@@ -16,6 +16,8 @@ var settings = function ($scope) {
 	});
 	$scope.applySettings = function () {
 		utils.saveStorage().next(function () {
+			$scope.$apply('save_success="fadeout"');
+			setTimeout($scope.$apply.bind($scope, 'save_success=""'), 2000);
 			if (!$scope.net_restart) {
 				return;
 			}
